3388075b80c6189551ad2e8f1e709e0254bb7e4b
Gabriel Perales <gabrielperales@gmail.com>
2016-09-28 15:21:12 +0100

    updated gulpfile

--- public/js/app.js
+++ public/js/app.js
@@ -18,17 +18,21 @@ var granimInstance = new Granim({
 });
 
 $(document).ready(function() {
+    var isMobile = function(){
+      var width = document.body.scrollWidth;
+      return width < 600;
+    };
 
     $('#fullpage').fullpage({
-
       anchors:['landing', 'info', 'option1', 'option2', 'option3'],
       onLeave: function(index, nextIndex, direction) {
 
         if(index == 1 && direction =='down') {
-          TweenMax.to('.fixed-header-links', 0.5, {css:{color: 'black', opacity: 0.5}})
+          TweenMax.to('.fixed-header-links', 0.5, {css:{color: 'black', opacity: 0.5}});
         } else if(index === 2 && direction === 'up') {
-          TweenMax.to('.fixed-header-links', 0.5, {css:{color: 'white', opacity: 1}})
+          TweenMax.to('.fixed-header-links', 0.5, {css:{color: 'white', opacity: 1}});
         }
-      }
-    })
+      },
+      responsiveWidth: 700
+    });
 });
